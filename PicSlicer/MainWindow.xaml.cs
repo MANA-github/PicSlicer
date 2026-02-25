68af6a6f9b1d5562252ea8914b82e11bdad530c8
MANA-github <153607046+MANA-github@users.noreply.github.com>
2026-02-25 12:05:16 +0900

    create repository

---
+++ PicSlicer/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MenuSample
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                Title = "画像を選択",
+                Filter = "画像 (*.png;*.jpg)|*.png;*.jpg|すべて (*.*)|*.*"
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                PreviewImage.Source = new BitmapImage(new Uri(dlg.FileName));
+            }
+        }
+
+        private void ToggleRuler_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+            => MessageBox.Show("保存が押されました");
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+            => Close();
+    }
+}
