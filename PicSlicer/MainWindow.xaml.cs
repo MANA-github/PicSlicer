201661f62b1d40af021f029957bcc3dd0dfb7683
MANA-github <153607046+MANA-github@users.noreply.github.com>
2026-03-10 19:35:06 +0900

    completed

--- PicSlicer/MainWindow.xaml.cs
+++ PicSlicer/MainWindow.xaml.cs
@@ -6,6 +6,10 @@ using System.Windows.Media.Imaging;
 using Syncfusion.UI.Xaml.Diagram.Controls;
 using Syncfusion.UI.Xaml.Diagram;
 using Syncfusion.Licensing;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.IO;
 
 
 namespace HomeMenu
@@ -28,6 +32,18 @@ namespace HomeMenu
             if (dlg.ShowDialog() == true)
             {
                 PreviewImage.Source = new BitmapImage(new Uri(dlg.FileName));
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (PreviewImage.Source is BitmapSource bitmap)
+            {
+                SaveImage(bitmap);
+            }
+            else
+            {
+                MessageBox.Show("保存する画像がありません。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
@@ -61,10 +77,52 @@ namespace HomeMenu
             }
         }
 
-        private void Save_Click(object sender, RoutedEventArgs e)
-            => MessageBox.Show("保存が押されました");
+
+        private void SaveImage(BitmapSource image)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+
+            dlg.Filter =
+                "PNG Image (*.png)|*.png|" +
+                "JPEG Image (*.jpg)|*.jpg|" +
+                "Bitmap Image (*.bmp)|*.bmp";
+
+            if (dlg.ShowDialog() != true)
+                return;
+
+            BitmapEncoder encoder;
+
+            string ext = Path.GetExtension(dlg.FileName).ToLower();
+
+            switch (ext)
+            {
+                case ".jpg":
+                case ".jpeg":
+                    encoder = new JpegBitmapEncoder()
+                    {
+                        QualityLevel = 90
+                    };
+                    break;
+
+                case ".bmp":
+                    encoder = new BmpBitmapEncoder();
+                    break;
+
+                default:
+                    encoder = new PngBitmapEncoder();
+                    break;
+            }
+
+            encoder.Frames.Add(BitmapFrame.Create(image));
+
+            using (FileStream stream = new FileStream(dlg.FileName, FileMode.Create))
+            {
+                encoder.Save(stream);
+            }
+        }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
             => Close();
     }
+
 }
